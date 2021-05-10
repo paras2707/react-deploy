9d3cddd6d07e87d41d0e9e02e93b02bb8eba8e71
paras2707 <pthapar_be20@thapar.edu>
2021-05-10 23:40:13 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import NavBar from './navbar'
+import Main from './main'
+import Footer from './footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 60,
+      item: {},
+      saving: 0
+    }
+  }
+
+  componentDidMount() {
+    this.myInterval = setInterval(() => {
+      this.setState(prevState => ({ count: prevState.count - 1 <= 0 ? 60 : prevState.count - 1 }))
+    }, 1000)
+
+    fetch("https://api.wazirx.com/api/v2/tickers")
+      .then(response => response.json())
+      .then(data => {
+        this.setState(() => ({
+          item: data.btcinr,
+          saving: data.btcinr.sell - data.btcinr.buy,
+          diff: (((data.btcinr.sell - data.btcinr.buy) / data.btcinr.buy) * 100).toFixed(2)
+        }))
+      })
+
+    this.fetchData = setInterval(() => {
+      fetch("https://api.wazirx.com/api/v2/tickers")
+        .then(response => response.json())
+        .then(data => {
+          this.setState(() => ({
+            item: data.btcinr,
+            saving: data.btcinr.sell - data.btcinr.buy,
+            diff: (((data.btcinr.sell - data.btcinr.buy) / data.btcinr.buy) * 100).toFixed(2)
+          }))
+        })
+    }, 60000)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NavBar counter={this.state.count} />
+        <Main item={this.state.item} saving={this.state.saving} diff={this.state.diff} />
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="flex">
+                <div className="footer-text">Copyright© 2019</div>
+                <div className="footer-text">HodlInfo.com</div>
+                <div className="footer-text">Developed By <span className="blue">QuadBTech</span></div>
+            </div>
+            <div>
+                <div className="footer-text">Support</div>
+            </div>
+        </footer>
+
+    )
+}
+
+export default Footer
